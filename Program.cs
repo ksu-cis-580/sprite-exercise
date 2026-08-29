30076c3aeb9ea788e9ce2c9ff6b3ac2524d5008b
Nathan Bean <nhbean@ksu.edu>
2026-08-28 21:55:43 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new SpriteExercise.SpriteExampleGame();
+game.Run();
---
+++ SpriteExampleGame.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace SpriteExercise;
+
+public class SpriteExampleGame : Game
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+
+    private SlimeGhostSprite _slimeGhost;
+
+    public SpriteExampleGame()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        // TODO: Add your initialization logic here
+        _slimeGhost = new();
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        // TODO: use this.Content to load your game content here
+        _slimeGhost.LoadContent(Content);
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        // TODO: Add your update logic here
+        _slimeGhost.Update(gameTime);
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        // TODO: Add your drawing code here
+        _spriteBatch.Begin();
+        _slimeGhost.Draw(gameTime, _spriteBatch);
+        _spriteBatch.End();
+
+        base.Draw(gameTime);
+    }
+}
